109187262e652c9eb5e4c59e4c514ed464075aeb
Daniel <daniel@vigilo.no>
2025-05-12 11:04:29 +0200

    Added GithubWorkflow (#3)
    Added pipeline to build test and publish the code

--- src/DevDanApi.Domain/Class1.cs
+++ src/DevDanApi.Domain/Class1.cs
@@ -1,6 +1,13 @@
 ﻿namespace DevDanApi.Domain;
 
+
 public class Class1
 {
+    private readonly string _test = "test";
+
+    public void Tests()
+    {
+        Console.WriteLine(_test);
+    }
 
 }
